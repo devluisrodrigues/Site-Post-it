2b2eaf1141a7e88ce30edf77544f8a1942a29ec4
Luis Rodrigues <luisasr@al.insper.edu.br>
2023-08-29 22:15:17 -0300

    night mode

--- templates/getit.js
+++ templates/getit.js
@@ -28,3 +28,47 @@ document.addEventListener("DOMContentLoaded", function () {
     )} card-rotation-${getRandomInt(1, 11)}`;
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+// Funcao Night Mode !!!
+var nightModeStatus = localStorage.getItem('night-mode')
+console.log(nightModeStatus)
+const nightModeToggle = document.getElementById('night-mode-toggle');
+
+const body = document.getElementById('body');
+const form = document.getElementById('form');
+const formTitle = document.getElementById('formTitle');
+const formDetails = document.getElementById('formDetails');
+const cards = document.getElementsByClassName('card');
+const cardTitle = document.getElementsByClassName('card-title');
+const cardContent = document.getElementsByClassName('card-content');
+
+if (nightModeStatus === 'true') {
+  console.log('nighto modo');
+  console.log(body.classList)
+  body.classList.toggle('night-mode');
+  form.classList.toggle('form-card-night-mode');
+  formTitle.classList.toggle('form-card-night-mode');
+  formDetails.classList.toggle('form-card-night-mode');
+  for(let i = 0; i < cards.length; i++) {
+    let colorNumber = cards[i].classList.item(1).split('-')[2];
+    console.log(colorNumber);
+    cards[i].classList.toggle(`card-color-${colorNumber}-night-mode`);
+  }
+  Array.from(cardTitle).forEach(el => el.classList.toggle('card-font-color-night-mode'));
+  Array.from(cardContent).forEach(el => el.classList.toggle('card-font-color-night-mode'));
+};
+
+nightModeToggle.addEventListener('click', () => {
+    if (nightModeStatus === 'false') {
+      localStorage.setItem('night-mode', 'true');  
+    } 
+    else if (nightModeStatus === null) {
+      localStorage.setItem('night-mode', 'true');
+    }
+    else {
+      localStorage.setItem('night-mode', 'false');
+    }
+    location.reload();
+});
+});
